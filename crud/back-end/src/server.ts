996aef74e9dfa43329867b0425c2b5b30fc4fb3a
Kauan Propodoski <kadupropodoski@gmail.com>
2025-06-17 22:34:59 -0300

    inicialização do projeto back-end

---
+++ crud/back-end/src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import { Request, Response, NextFunction } from 'express';
+
+const app = express();
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+interface errorType{
+        message: string
+        status: number
+}
+
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const erro:errorType = {
+        message: 'Route not found',
+        status: 404
+    }
+    next(erro)
+})
+
+app.use((erro: errorType, req:Request, res: Response, next: NextFunction) => {
+    res.status(erro.status || 500);
+    res.send({ error: { message: erro.message }})
+})
+
+
+app.listen(3001, () => 'server running on port 3001')
+
