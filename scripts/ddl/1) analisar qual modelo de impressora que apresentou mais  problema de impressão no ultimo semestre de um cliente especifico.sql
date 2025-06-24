4a67362352b3e16d20ac94308fe791471601c82f
gumarazzi23 <gumarazzi@hotmail.com>
2025-06-23 23:53:59 -0300

    Analisar qual modelo de impressora que apresentou mais  problema de impressão no ultimo semestre de um cliente especifico.sql
    Código para consultar a informação desejada

---
+++ scripts/ddl/1) analisar qual modelo de impressora que apresentou mais  problema de impressão no ultimo semestre de um cliente especifico.sql
@@ -0,0 +1,10 @@
+SELECT mo.nome_modelo, COUNT(*) AS total_problemas
+FROM contratos c
+JOIN produtos p ON c.cd_produto = p.cd_produto
+JOIN manutencoes m ON m.cd_produto = p.cd_produto
+JOIN modelos mo ON p.cd_modelo = mo.cd_modelo
+WHERE c.cd_cliente = ? -- substituir pelo código do cliente desejado.
+  AND m.data_manutencao >= CURRENT_DATE - 6 MONTHS
+GROUP BY mo.nome_modelo
+ORDER BY total_problemas DESC
+FETCH FIRST 1 ROW ONLY;
