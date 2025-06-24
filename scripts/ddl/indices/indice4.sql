d3f20116f3971c30dc7ee1be60347ba4f4a56cef
LeonardoSilvaJoaquim11 <164394785+LeonardoSilvaJoaquim11@users.noreply.github.com>
2025-06-24 01:04:17 -0300

    Update indice4.sql

--- scripts/ddl/indices/indice4.sql
+++ scripts/ddl/indices/indice4.sql
@@ -1,9 +1,9 @@
 -- Índice para a coluna nome_marca da tabela marcas
-CREATE NONCLUSTERED INDEX idx_marcas_nome_marca
+CREATE INDEX idx_marcas_nome_marca
 ON marcas (nome_marca);
 GO
 
 -- Índice para a coluna nome_status da tabela status
-CREATE NONCLUSTERED INDEX idx_status_nome_status
+CREATE INDEX idx_status_nome_status
 ON status (nome_status);
 GO
